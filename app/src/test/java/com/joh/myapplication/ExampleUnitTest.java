aabae3ef177d42e248569c78315a36e873ff83ff
joh liu <liuheng@chinayasha.com>
2019-06-13 15:58:59 +0800

    readme

--- app/src/test/java/com/joh/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/joh/myapplication/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.joh.demo.scrowviewdemo;
+package com.joh.myapplication;
 
 import org.junit.Test;
 
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
+    public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
 }
